a9a568b23ca685b0340c805ba7bdcd29f36e858b
satendra-patel <76507241+satendra-patel@users.noreply.github.com>
2022-12-21 02:20:27 +0530

    Add files via upload

---
+++ task3/i.js
@@ -0,0 +1,4 @@
+let c=document.getElementById("main-header");
+c.style.border="solid 4px black";
+document.getElementById("item99").style.color="green";
+document.getElementById("item99").style.fontStyle="italic";
