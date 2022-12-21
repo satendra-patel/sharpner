187dd508d1dc8472219b2eb27020b648090ce87e
satendra-patel <patelsatendra06@gmail.com>
2022-12-21 14:59:37 +0530

    task4

---
+++ task4/4.js
@@ -0,0 +1,6 @@
+let c=document.getElementsByClassName("list-group-item");
+let d=document.getElementsByClassName("list-group")[0];
+ c[2].style.color='green';
+
+d.style.fontFamily = "Impact,Charcoal,sans-serif";
+
