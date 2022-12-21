247fff24ef7d96a7b6b7ac82e61fbee8501ab66c
satendra-patel <patelsatendra06@gmail.com>
2022-12-21 15:16:45 +0530

    task5

---
+++ task5/5.js
@@ -0,0 +1,5 @@
+let c=document.getElementsByClassName("list-group-items")[0];
+c.style.color="red";
+document.getElementsByTagName("li")[2].style.backgroundColor="blue";
+document.getElementsByTagName("li")[0].style.fontStyle="italic";
+document.getElementsByTagName("ul")[0].style.backgroundColor="cyan"
