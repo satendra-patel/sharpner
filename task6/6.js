5c8ec3aeb508ed395af5cc696d861a2531bcd8e4
satendra-patel <patelsatendra06@gmail.com>
2022-12-21 15:34:22 +0530

    task5

---
+++ task6/6.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".list-group-item")[1].style.backgroundColor="green";
+var od= document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<od.length;i++){
+    od[i].style.backgroundColor="red";
+}
+var even=document.querySelectorAll('li:nth-child(even)');
+for(var i=0;i<even.length;i++)
+{
+    even[i].style.backgroundColor="cyan";
+}
