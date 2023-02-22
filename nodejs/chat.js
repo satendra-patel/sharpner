8317f1201ece46240fcf461fdadbd8f69ca9f7c6
satendra-patel <patelsatendra06@gmail.com>
2023-02-23 02:03:37 +0530

    task

--- nodejs/chat.js
+++ nodejs/chat.js
@@ -7,35 +7,49 @@ const server=http.createServer((req,res)=>{
     const method=req.method;
     if(url==='/')
     {
-        res.write('<html>');
+        
+        fs.readFile("message.txt",{encoding:'utf-8'},(data,err)=>{
+            if(err){
+                console.log(err);
+            }
+            res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" ><button type="submit">Send</button></form></body>');
+        res.write(`<body>${data}</body>`);
+        res.write('<body>$<data><form action="/message" method="POST"><input type="text" ><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
-    }
-    if(url==='/message' && method==='POST'){
-        const body=[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
         });
-        req.on('end',()=>{
+         
+    }
+    else if(url==="/message" && method==="POST"){
+        let body=[];
+        req.on('data',(chunk)=>{
+           
+            body.push(chunk);
+
+        });
+        return req.on("end",()=>{
+            
             const parsedBody=Buffer.concat(body).toString();
-            const message=parsedBody.split('=')[1];
-            console.log(message);
-            fs.writeFileSync('message.txt',message);
+            const message=parsedBody.split("=")[1];
+           
+            
+           fs.writeFile('message.txt', message,(err)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log(`indise fs.writefile`);
             res.statusCode=302;
             res.setHeader('location','/');
             return res.end();
+            }); 
         });
-       
-       
-
+    }else{
+        res.setHeader("Content-Type","text/html");
+        res.write("page not found");
+        res.end();
     }
-    res.write('Hello Guys');
 
 
 });
-server.listen(4000, () => {
-    console.log('Satendra Patel');
-  });
+server.listen(4000);
