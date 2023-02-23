6c90be7eef9d174cc2df0d231c35c7f01d941a1a
satendra-patel <patelsatendra06@gmail.com>
2023-02-23 15:17:51 +0530

    chat aap working properly

--- nodejs/chat.js
+++ nodejs/chat.js
@@ -1,55 +1,9 @@
-const http=require('http');
-const fs=require('fs');
-const { buffer } = require('stream/consumers');
+const http = require('http');
 
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    if(url==='/')
-    {
-        
-        fs.readFile("message.txt",{encoding:'utf-8'},(data,err)=>{
-            if(err){
-                console.log(err);
-            }
-            res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write(`<body>${data}</body>`);
-        res.write('<body>$<data><form action="/message" method="POST"><input type="text" ><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-        });
-         
-    }
-    else if(url==="/message" && method==="POST"){
-        let body=[];
-        req.on('data',(chunk)=>{
-           
-            body.push(chunk);
+const routes = require('./route');
 
-        });
-        return req.on("end",()=>{
-            
-            const parsedBody=Buffer.concat(body).toString();
-            const message=parsedBody.split("=")[1];
-           
-            
-           fs.writeFile('message.txt', message,(err)=>{
-            if(err){
-                console.log(err);
-            }
-            console.log(`indise fs.writefile`);
-            res.statusCode=302;
-            res.setHeader('location','/');
-            return res.end();
-            }); 
-        });
-    }else{
-        res.setHeader("Content-Type","text/html");
-        res.write("page not found");
-        res.end();
-    }
+console.log(routes.someText);
 
+const server = http.createServer(routes.handler);
 
-});
 server.listen(4000);
