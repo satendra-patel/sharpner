e340ec96619a194677fe965e152b96f92ea94f55
satendra-patel <patelsatendra06@gmail.com>
2023-02-25 01:35:44 +0530

    with post request

--- express/app.js
+++ express/app.js
@@ -1,19 +1,21 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const express=require('express');
+const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-const app=express();
-
-app.use((req,res,next)=>{
-    console.log("inside the middleware");
-    next();
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
-app.use((req,res,next)=>{
-    console.log("inside another middleware");
-    res.send(`<h2>hello from express</h2>`);
-})
 
-const server = http.createServer(app);
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
