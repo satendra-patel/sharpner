96833d150d6fc7143ed4b0470ec9d1c31a9d5692
satendra-patel <patelsatendra06@gmail.com>
2023-02-25 23:48:28 +0530

    admin page with contactus

--- express/routes/contactus.js
+++ express/routes/contactus.js
@@ -6,8 +6,12 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+// /admin/add-product => GET
+router.get('/contactus', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'contactus.html'));
 });
+
+// /admin/add-product => POST
+
 
 module.exports = router;
--- express/routes/success.js
+++ express/routes/success.js
@@ -7,14 +7,12 @@ const rootDir = require('../util/path');
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.post('/success', (req, res, next) => {
+    console.log(req.body);
+  res.sendFile(path.join(rootDir, 'views', 'success.html'));
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
+
 
 module.exports = router;
